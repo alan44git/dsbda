d0cb525b591b6d768524daadb3a4c8e3cc9e879b
alan44git <alan44.git@gmail.com>
2026-05-13 06:14:53 +0530

    d29

--- d29/main.py
+++ d29/main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 import time
 
 options = webdriver.ChromeOptions()
@@ -15,53 +16,35 @@ driver = webdriver.Chrome(
     options=options
 )
 
-driver.get("https://www.myntra.com/reviews/30141373")
-time.sleep(5)
+driver.get("https://pict.edu")
+time.sleep(10)
 
 WebDriverWait(driver,10).until(
     EC.presence_of_element_located(
-        (By.CSS_SELECTOR, 'div.user-review-userReviewWrapper')
+        (By.CLASS_NAME, 'testimonial-item')
     )
 )
 
-review_divs = driver.find_elements(By.CSS_SELECTOR, 'div.user-review-userReviewWrapper')
+items = driver.find_elements(By.CLASS_NAME, 'testimonial-item')
 
-reviews = []
+testimonials = []
 
-for div in review_divs:
-    review = {}
-
+for item in items:
+    testimonial = {}
     try:
-        review["rating"] = div.find_element(By.CLASS_NAME,"user-review-starWrapper").text
+        testimonial["name"] = item.find_element(By.CLASS_NAME, 'testi-name').text.strip()
     except:
-        review["rating"] = None
-
+        testimonial["name"] = None
+        
     try:
-        review["text"] = div.find_element(By.CLASS_NAME,"user-review-reviewTextWrapper").text 
+        testimonial["text"] = item.find_element(By.CLASS_NAME, 'overflow-testimonial').text.strip()
     except:
-        review["text"] = None
+        testimonial["text"] = None
+    
+    testimonials.append(testimonial)
 
-    try:
-        review["name"] = div.find_element(By.CSS_SELECTOR,'div.user-review-left span:first-child').text 
-    except:
-        review["name"] = None
+print(testimonials)
 
-    try:
-        review["date"] = div.find_element(By.CSS_SELECTOR,'div.user-review-left span:last-child').text 
-    except:
-        review["date"] = None
-
-    try:
-        review["likes"] = div.find_elements(By.CLASS_NAME,'user-review-thumb')[0].text 
-    except:
-        review["likes"] = None
-
-    try:
-        review["dislikes"] = div.find_elements(By.CLASS_NAME,'user-review-thumb')[1].text 
-    except:
-        review["dislikes"] = None
-
-    reviews.append(review)
-
-print(reviews)
+df = pd.DataFrame(testimonials)
+df.to_csv('testimonials.csv', index=False)
 
